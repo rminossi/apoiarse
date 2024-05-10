f7c42b75e20169d8c290310bd5dd70d8a9b6125e
Rafael Minossi <rafaelminossi@gmail.com>
2024-05-09 23:43:56 -0300

    remove sandbox

--- app/Services/AsaasService.php
+++ app/Services/AsaasService.php
@@ -31,7 +31,7 @@ class AsaasService
     public function createCustomer($name, $document, $email, $phone, $id)
     {
 
-        $response = $this->client->request('POST', 'https://sandbox.asaas.com/api/v3/customers', [
+        $response = $this->client->request('POST', 'https://asaas.com/api/v3/customers', [
             'body' => '{"name":"' . $name . '","cpfCnpj":"' . $document . '","email":"' . $email . '","phone":"' . $phone . '","externalReference":"' . $id . '"}',
             'headers' => [
                 'accept' => 'application/json',
@@ -44,7 +44,7 @@ class AsaasService
 
     public function getCustomer($id)
     {
-        $response = $this->client->request('GET', 'https://sandbox.asaas.com/api/v3/users/' . $id, [
+        $response = $this->client->request('GET', 'https://asaas.com/api/v3/users/' . $id, [
             'headers' => [
                 'accept' => 'application/json',
                 'access_token' => $this->token,
@@ -58,7 +58,7 @@ class AsaasService
 
     public function updateCustomer($id, $name, $phone, $email)
     {
-        $response = $this->client->request('PUT', 'https://sandbox.asaas.com/api/v3/users/' . $id, [
+        $response = $this->client->request('PUT', 'https://asaas.com/api/v3/users/' . $id, [
             'body' => '{"name":"' . $name . '","email":"' . $email . '","phone":"' . $phone . '"}',
             'headers' => [
                 'accept' => 'application/json',
@@ -86,7 +86,7 @@ class AsaasService
     //Pix
     public function gerarPixQrCode($id)
     {
-        $response = $this->client->request('POST', 'https://sandbox.asaas.com/api/v3/payments/' . $id . '/pixQrCode', [
+        $response = $this->client->request('POST', 'https://asaas.com/api/v3/payments/' . $id . '/pixQrCode', [
             'headers' => [
                 'accept' => 'application/json',
                 'access_token' => $this->token,
@@ -101,7 +101,7 @@ class AsaasService
     //Cobranças
     public function criarCobrancaPix($id, $valor, $descricao, $data_vencimento)
     {
-        $response = $this->client->request('POST', 'https://sandbox.asaas.com/api/v3/payments', [
+        $response = $this->client->request('POST', 'https://asaas.com/api/v3/payments', [
             'body' => '{"customer":"' . $id . '","value":"' . $valor . '","description":"' . $descricao . '", "billingType":"PIX","dueDate":"' . $data_vencimento . '"}',
             'headers' => [
                 'accept' => 'application/json',
@@ -134,7 +134,7 @@ class AsaasService
     {
         $data_vencimento = now()->format('Y-m-d');
         try {
-            $response = $this->client->request('POST', 'https://sandbox.asaas.com/api/v3/payments', [
+            $response = $this->client->request('POST', 'https://asaas.com/api/v3/payments', [
                 'body' => '{"customer":"' . $asaas_id . '", "billingType":"CREDIT_CARD", "value":"' . $valor . '","description":"' . $descricao . '", "dueDate":"' . $data_vencimento . '", "creditCard":{ "number":"' . $cardNumber . '", "holderName":"' . $cardHolderName . '", "expiryMonth":"' . $cardMonth . '", "expiryYear":"' . $cardYear . '", "ccv":"' . $cardCvv . '"}, "creditCardHolderInfo":{ "name":"' . $cardHolderName . '", "phone":"' . $cardPhone . '", "email":"' . $cardEmail . '", "cpfCnpj":"' . $cardCpfCnpj . '", "postalCode":"' . $cardPostalCode . '", "addressNumber":"' . $cardAddressNumber . '", "addressComplement":"' . $cardAddressComplement . '"}, "remoteIp":"' . $remoteIp . '"}',
                 'headers' => [
                     'accept' => 'application/json',
