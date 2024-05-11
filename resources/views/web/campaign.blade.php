71b9099fbe585d7b807360883ba2e409aba6508b
Rafael Minossi <rafaelminossi@gmail.com>
2024-05-11 15:42:52 -0300

    donations accordeon

--- resources/views/web/campaign.blade.php
+++ resources/views/web/campaign.blade.php
@@ -209,7 +209,7 @@
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        @foreach($campaign->donations as $index => $donation)
+                                        @foreach($campaign->totalDonations as $index => $donation)
                                             <tr class="text-center" style="background-color: {{ $index % 2 == 0 ? '#f8f9fa' : 'white' }};">
                                                 <td style="border: 1px solid #dee2e6; max-width: 200px; word-wrap: break-word; white-space: normal;">
                                                     {{ explode(' ', trim($donation->user->name))[0] }} ********
