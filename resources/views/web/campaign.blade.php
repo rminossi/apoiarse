557fbb990fdd89483bfa2a1e17c2b6a176cf62e3
Rafael Minossi <rafaelminossi@gmail.com>
2024-05-11 17:05:58 -0300

    donations accordeon

--- resources/views/web/campaign.blade.php
+++ resources/views/web/campaign.blade.php
@@ -212,7 +212,7 @@
                                         @foreach($campaign->donations()->where('status', 3)->get() as $index => $donation)
                                             <tr class="text-center" style="background-color: {{ $index % 2 == 0 ? '#f8f9fa' : 'white' }};">
                                                 <td style="border: 1px solid #dee2e6; max-width: 200px; word-wrap: break-word; white-space: normal;">
-                                                    {{ explode(' ', trim($donation->user->name))[0] }} ********
+                                                    {{ explode(' ', trim($donation->user->name))[0] }}
                                                 </td>
                                                 <td style="border: 1px solid #dee2e6;">
                                                     {{ explode('.', trim($donation->user->cpf))[0] }}.***.***-**
