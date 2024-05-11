c139b1df3390e562b35d052558207b3c061de077
Rafael Minossi <rafaelminossi@gmail.com>
2024-05-11 09:42:49 -0300

    campaign card fixes

--- resources/views/web/campaigns.blade.php
+++ resources/views/web/campaigns.blade.php
@@ -1,102 +1,111 @@
 @extends('web.master.master')
 @section('content')
-<div class="fh5co-hero fh5co-hero-2">
-    <div class="fh5co-overlay"></div>
-    <div class="fh5co-cover fh5co-cover_2 text-center" data-stellar-background-ratio="0.5" style="background-image: url('{{asset('assets/frontend/images/apoiarse.png')}}');">
-        <div class="desc animate-box">
-            <h2><strong>Nossas Campanhas</strong></h2>
-            <span><strong>Confira abaixo as campanhas atuais e também as finalizadas.</strong></span>
+    <div class="fh5co-hero fh5co-hero-2">
+        <div class="fh5co-overlay"></div>
+        <div class="fh5co-cover fh5co-cover_2 text-center" data-stellar-background-ratio="0.5"
+             style="background-image: url('{{asset('assets/frontend/images/apoiarse.png')}}');">
+            <div class="desc animate-box">
+                <h2><strong>Nossas Campanhas</strong></h2>
+                <span><strong>Confira abaixo as campanhas atuais e também as finalizadas.</strong></span>
+            </div>
         </div>
     </div>
-</div>
-<!-- end:header-top -->
-<div id="fh5co-portfolio">
-    <div class="container">
-        @if(sizeof($activeCampaigns) > 0)
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
-                <h3>Campanhas Ativas</h3>
-                <p>Confira nossas campanhas ativas, o que significa que ainda dá tempo de apoiar!</p>
-            </div>
-        </div>
-        @isset($activeCampaigns)
-        <div class="row row-bottom-padded-md">
-            <div class="col-md-12">
-                <ul id="fh5co-portfolio-list">
-                    @foreach($activeCampaigns as $campaign)
-                    <div class="col-lg-4 col-md-4">
-                        <div class="fh5co-blog animate-box">
-                            <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
-                            <div class="blog-text">
-                                <div class="prod-title">
-                                    <h3><a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a></h3>
-                                    <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}" class="btn btn-primary p-3 col-12">{{"Apoiar!"}}</a>
-                                </div>
-                            </div>
+    <!-- end:header-top -->
+    <div id="fh5co-portfolio">
+        <div class="container">
+            @if(sizeof($activeCampaigns) > 0)
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
+                        <h3>Campanhas Ativas</h3>
+                        <p>Confira nossas campanhas ativas, o que significa que ainda dá tempo de apoiar!</p>
+                    </div>
+                </div>
+                @isset($activeCampaigns)
+                    <div class="row row-bottom-padded-md">
+                        <div class="col-md-12">
+                            <ul id="fh5co-portfolio-list">
+                                @foreach($activeCampaigns as $campaign)
+                                    <div class="col-lg-4 col-md-4">
+                                        <div class="fh5co-blog animate-box">
+                                            <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img
+                                                    class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
+                                            <div class="blog-text">
+                                                <div class="prod-title">
+                                                    <h3 style="height:150px">
+                                                        <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a>
+                                                    </h3>
+                                                    <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"
+                                                       class="btn btn-primary p-3 col-12">{{"Apoiar!"}}</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </ul>
                         </div>
                     </div>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-        @endisset
-        @endif
-        @if(sizeof($finishedCampaigns) > 0)
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
-                <h3>Campanhas Encerradas</h3>
-                <p>Confira nossas campanhas encerradas.</p>
-            </div>
-        </div>
-        @isset($finishedCampaigns)
-        <div class="row row-bottom-padded-md">
-            <div class="col-md-12">
-                <ul id="fh5co-portfolio-list">
-                    @foreach($finishedCampaigns as $campaign)
-                    <div class="col-lg-4 col-md-4">
-                        <div class="fh5co-blog animate-box">
-                            <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
-                            <div class="blog-text">
-                                <div class="prod-title">
-                                    <h3><a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a></h3>
-                                    <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}" class="btn btn-primary p-3 col-12">{{"Ver Campanha"}}</a>
-                                </div>
-                            </div>
+                @endisset
+            @endif
+            @if(sizeof($finishedCampaigns) > 0)
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
+                        <h3>Campanhas Encerradas</h3>
+                        <p>Confira nossas campanhas encerradas.</p>
+                    </div>
+                </div>
+                @isset($finishedCampaigns)
+                    <div class="row row-bottom-padded-md">
+                        <div class="col-md-12">
+                            <ul id="fh5co-portfolio-list">
+                                @foreach($finishedCampaigns as $campaign)
+                                    <div class="col-lg-4 col-md-4">
+                                        <div class="fh5co-blog animate-box">
+                                            <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img
+                                                    class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
+                                            <div class="blog-text">
+                                                <div class="prod-title">
+                                                    <h3 style="height:150px">
+                                                        <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a>
+                                                    </h3>
+                                                    <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"
+                                                       class="btn btn-primary p-3 col-12">{{"Ver Campanha"}}</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </ul>
                         </div>
                     </div>
-                    @endforeach
-                </ul>
-            </div>
+                @endisset
+            @endif
         </div>
-        @endisset
-        @endif
     </div>
-</div>
-<script>
-    // Get the modal
-    var modal = document.getElementById("myModal");
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+        // When the user clicks on the button, open the modal
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
             modal.style.display = "none";
         }
-    }
-</script>
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
 @endsection
--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,80 +1,89 @@
 @extends('web.master.master')
 @section('content')
-<div class="fh5co-hero">
-	<div class="fh5co-overlay"></div>
-	<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url({{ asset('assets/frontend/images/apoiarse.png') }});">
-		<div class="desc animate-box">
-			<h2><strong>Venha apoiar conosco</strong></h2>
-			<span><a class="btn btn-primary btn-lg" href="{{route('web.campaigns')}}">Ver nossas campanhas</a></span>
-		</div>
-	</div>
-</div>
-@isset($activeCampaigns)
-<div id="fh5co-blog-section" class="fh5co-section-gray">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-				<h3>Últimas campanhas</h3>
-				<p>Confira aqui as últimas campanhas cadastradas e apoie você também!</p>
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row row-bottom-padded-md">
-			@foreach($activeCampaigns as $campaign)
-			<div class="col-lg-4 col-md-4">
-				<div class="fh5co-blog animate-box">
-					<a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
-					<div class="blog-text">
-						<div class="prod-title">
-							<h3><a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a></h3>
-							<a href="{{route('web.campaign', ['slug' => $campaign->slug])}}" class="btn btn-primary p-3 col-12">{{"Apoiar!"}}</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			@endforeach
-		</div>
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4 text-center animate-box">
-				<a href="{{route('web.campaigns')}}" class="btn btn-primary btn-lg">Ver Todas</a>
-			</div>
-		</div>
-	</div>
-</div>
-@endisset
-@if(sizeof($finishedCampaigns) > 0)
-<div id="fh5co-blog-section" class="fh5co-section-gray">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
-				<h3>Sorteios encerrados</h3>
-				<p>Veja nossos últimos sorteios realizados, seus detalhes e ganhadores.</p>
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row row-bottom-padded-md">
-			@foreach($finishedCampaigns as $campaign)
-			<div class="col-lg-4 col-md-4">
-				<div class="fh5co-blog animate-box">
-					<a href=""><img class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
-					<div class="blog-text">
-						<div class="prod-title">
-							<h3><a href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a></h3>
-							<a href="{{route('web.campaign', ['slug' => $campaign->slug])}}" class="btn btn-primary p-3 col-12">{{"Ver Campanha"}}</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			@endforeach
-		</div>
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4 text-center animate-box">
-				<a href="" class="btn btn-primary btn-lg">Ver Todas</a>
-			</div>
-		</div>
-	</div>
-</div>
-@endif
+    <div class="fh5co-hero">
+        <div class="fh5co-overlay"></div>
+        <div class="fh5co-cover text-center" data-stellar-background-ratio="0.5"
+             style="background-image: url({{ asset('assets/frontend/images/apoiarse.png') }});">
+            <div class="desc animate-box">
+                <h2><strong>Venha apoiar conosco</strong></h2>
+                <span><a class="btn btn-primary btn-lg"
+                         href="{{route('web.campaigns')}}">Ver nossas campanhas</a></span>
+            </div>
+        </div>
+    </div>
+    @isset($activeCampaigns)
+        <div id="fh5co-blog-section" class="fh5co-section-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                        <h3>Últimas campanhas</h3>
+                        <p>Confira aqui as últimas campanhas cadastradas e apoie você também!</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row row-bottom-padded-md">
+                    @foreach($activeCampaigns as $campaign)
+                        <div class="col-lg-4 col-md-4">
+                            <div class="fh5co-blog animate-box">
+                                <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"><img
+                                        class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
+                                <div class="blog-text">
+                                    <div class="prod-title">
+                                        <h3 style="height:150px"><a
+                                                href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a>
+                                        </h3>
+                                        <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"
+                                           class="btn btn-primary p-3 col-12">{{"Apoiar!"}}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 text-center animate-box">
+                        <a href="{{route('web.campaigns')}}" class="btn btn-primary btn-lg">Ver Todas</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endisset
+    @if(sizeof($finishedCampaigns) > 0)
+        <div id="fh5co-blog-section" class="fh5co-section-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                        <h3>Sorteios encerrados</h3>
+                        <p>Veja nossos últimos sorteios realizados, seus detalhes e ganhadores.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row row-bottom-padded-md">
+                    @foreach($finishedCampaigns as $campaign)
+                        <div class="col-lg-4 col-md-4">
+                            <div class="fh5co-blog animate-box">
+                                <a href=""><img class="img-responsive" src="{{url($campaign->cover())}}" alt=""></a>
+                                <div class="blog-text">
+                                    <div class="prod-title">
+                                        <h3 style="height:150px"><a
+                                                href="{{route('web.campaign', ['slug' => $campaign->slug])}}">{{$campaign->title}}</a>
+                                        </h3>
+                                        <a href="{{route('web.campaign', ['slug' => $campaign->slug])}}"
+                                           class="btn btn-primary p-3 col-12">{{"Ver Campanha"}}</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 text-center animate-box">
+                        <a href="" class="btn btn-primary btn-lg">Ver Todas</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 @endsection
